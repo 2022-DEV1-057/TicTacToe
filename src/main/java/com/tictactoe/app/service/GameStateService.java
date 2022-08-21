6a2f29867a0f40aab407fdecaebc59a54a2649d0
2022-DEV1-057 <2022dev1057@gmail.com>
2022-08-22 00:49:22 +0530

    feat: New game board ready

--- src/main/java/com/tictactoe/app/service/GameStateService.java
+++ src/main/java/com/tictactoe/app/service/GameStateService.java
@@ -1,5 +1,8 @@
 package com.tictactoe.app.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -13,6 +16,17 @@ public class GameStateService implements TictactoeApiDelegate {
 	@Override
 	public ResponseEntity<NewGameInfo> startNewGame() {
 		NewGameInfo newGameInfo= new NewGameInfo();
+		Map<String, String> gameBoard = new HashMap<>();
+		gameBoard.put("1", null);
+		gameBoard.put("2", null);
+		gameBoard.put("3", null);
+		gameBoard.put("4", null);
+		gameBoard.put("5", null);
+		gameBoard.put("6", null);
+		gameBoard.put("7", null);
+		gameBoard.put("8", null);
+		gameBoard.put("9", null);
+		newGameInfo.setGameboard(gameBoard);
 		return new ResponseEntity<>(newGameInfo, HttpStatus.CREATED);
 	}
 }
--- src/test/java/com/tictactoe/app/service/GameStateServiceTest.java
+++ src/test/java/com/tictactoe/app/service/GameStateServiceTest.java
@@ -1,13 +1,23 @@
 package com.tictactoe.app.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tictactoe.app.openapi.model.NewGameInfo;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -18,5 +28,29 @@ public class GameStateServiceTest {
 	@Test
 	public void checkStartNewGameServiceAvailable() throws Exception {
 		this.mockMvc.perform(post("/tictactoe/startNewGame")).andExpect(status().is(201));
+	}
+
+	@Test
+	public void checkNewGameBoardReady() throws Exception {
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/tictactoe/startNewGame");
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+		String responseBody = result.getResponse().getContentAsString();
+		ObjectMapper objectMapper = new ObjectMapper();
+		NewGameInfo gameBoardInfo = objectMapper.readValue(responseBody, NewGameInfo.class);
+		assertEquals(getDefaultGameBoard(), gameBoardInfo.getGameboard());
+	}
+
+	public Map<String, String> getDefaultGameBoard() {
+		Map<String, String> defaultGameBoard = new HashMap<>();
+		defaultGameBoard.put("1", null);
+		defaultGameBoard.put("2", null);
+		defaultGameBoard.put("3", null);
+		defaultGameBoard.put("4", null);
+		defaultGameBoard.put("5", null);
+		defaultGameBoard.put("6", null);
+		defaultGameBoard.put("7", null);
+		defaultGameBoard.put("8", null);
+		defaultGameBoard.put("9", null);
+		return defaultGameBoard;
 	}
 }
