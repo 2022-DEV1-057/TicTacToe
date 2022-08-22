6ea2dad875f72e8df0e9883547b75be69c81b96c
2022-DEV1-057 <2022dev1057@gmail.com>
2022-08-22 13:22:05 +0530

    feat: Logging added

--- src/main/java/com/tictactoe/app/TicTacToeApplication.java
+++ src/main/java/com/tictactoe/app/TicTacToeApplication.java
@@ -1,13 +1,17 @@
 package com.tictactoe.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TicTacToeApplication {
+	private static Logger logger = LoggerFactory.getLogger(TicTacToeApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(TicTacToeApplication.class, args);
+		logger.info("--: TicTacToe game rest api service started! :-");
 	}
 
 }
